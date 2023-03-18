13796f3240be680118a2dfda9967c4803c0c7fec
Vaibhav Bhawsar <Vaibhav.bhawsar.77@gmail.com>
2023-03-18 22:47:59 +0530

    change console from smtp

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -150,7 +150,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Setting Email Backend with smtp django
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
